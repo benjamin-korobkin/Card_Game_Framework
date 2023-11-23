2bcf8016d1aac24f76254058e992189563d308c4
benjamin-korobkin <benjamin.korobkin@gmail.com>
2023-11-23 15:33:17 +0200

    first

---
+++ src/new_card_game/cards/sets/SetDefinition_Core.gd
@@ -0,0 +1,17 @@
+extends Reference
+
+const SET = "Core Set"
+const CARDS := {
+	"Yochanan ben Zakkai": {
+		"Type": "Figure",
+		"Cost": 5,
+		"Power": 5,
+		"Era": "Tanna",
+	},
+	"Rav Yehudah": {
+		"Type": "Figure",
+		"Cost": 5,
+		"Power": 4,
+		"Era": "Amora",
+	},
+}
