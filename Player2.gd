16157a2d4d908541939cc21a22c3c823effbef7c
benjamin-korobkin <benjamin.korobkin@gmail.com>
2023-12-04 09:18:17 +0200

    start setup of 2 players

---
+++ Player2.gd
@@ -0,0 +1,5 @@
+extends "res://src/new_card_game/Player.gd"
+
+
+func _ready() -> void:
+	pass
---
+++ src/new_card_game/Player.gd
@@ -0,0 +1,12 @@
+class_name Player
+extends Node2D
+
+var cards : Array
+# var cards_amt = cards.size()
+var actions_remaining : int
+var torah_tokens : int
+var is_turn : bool  # flag whether it's this player's turn
+
+
+func _ready() -> void:
+	pass
