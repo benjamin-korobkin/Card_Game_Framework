950851fee8fb3022d72278e1711165c5f7b31253
benjamin-korobkin <benjamin.korobkin@gmail.com>
2023-12-04 14:08:43 +0200

    added 2nd hand

--- src/new_card_game/Hand2.gd
+++ src/new_card_game/Hand2.gd
@@ -4,3 +4,4 @@ extends Hand
 func _ready() -> void:
 	# warning-ignore:return_value_discarded
 	$Control/ManipulationButtons/DiscardRandom.connect("pressed",self,'_on_DiscardRandom_Button_pressed')
+
--- src/new_card_game/Player1.gd
+++ src/new_card_game/Player1.gd
@@ -1,5 +1,6 @@
 extends Player
 
+export (PackedScene) var hand_scene : PackedScene
 
 func _ready() -> void:
 	pass
