406f5ed1e50ee9e0c364e6f076dc4d71ee3ad02a
benjamin-korobkin <benjamin.korobkin@gmail.com>
2024-01-12 14:20:47 +0200

    added more cards

--- src/new_card_game/cards/sets/SetDefinition_Core.gd
+++ src/new_card_game/cards/sets/SetDefinition_Core.gd
@@ -52,6 +52,106 @@ const CARDS := {
 		"Power": 1,
 		"Era": "Acharon",
 	},
+		"Rebbi Akiva": {
+		"Type": "Sage",
+		"Power": 5,
+		"Era": "Tanna",
+	},
+	"Shmuel": {
+		"Type": "Sage",
+		"Power": 4,
+		"Era": "Amora",
+	},
+	"Rav Mari b Rav Dimi": {
+		"Type": "Sage",
+		"Power": 3,
+		"Era": "Gaon",
+	},
+	"Ramban": {
+		"Type": "Sage",
+		"Power": 2,
+		"Era": "Rishon",
+	},
+	"Sforno": {
+		"Type": "Sage",
+		"Power": 1,
+		"Era": "Acharon",
+	},
+	"Rebbi Yishmael": {
+		"Type": "Sage",
+		"Power": 5,
+		"Era": "Tanna",
+	},
+	"Rav Huna": {
+		"Type": "Sage",
+		"Power": 4,
+		"Era": "Amora",
+	},
+	"Rav Ra'ava": {
+		"Type": "Sage",
+		"Power": 3,
+		"Era": "Gaon",
+	},
+	"R Yehuda HaLevi": {
+		"Type": "Sage",
+		"Power": 2,
+		"Era": "Rishon",
+	},
+	"Rema": {
+		"Type": "Sage",
+		"Power": 1,
+		"Era": "Acharon",
+	},
+	"Rebbi Meir": {
+		"Type": "Sage",
+		"Power": 5,
+		"Era": "Tanna",
+	},
+	"Rava": {
+		"Type": "Sage",
+		"Power": 4,
+		"Era": "Amora",
+	},
+	"Rav Bustenai": {
+		"Type": "Sage",
+		"Power": 3,
+		"Era": "Gaon",
+	},
+	"Ibn Ezra": {
+		"Type": "Sage",
+		"Power": 2,
+		"Era": "Rishon",
+	},
+	"Arizal": {
+		"Type": "Sage",
+		"Power": 1,
+		"Era": "Acharon",
+	},
+	"Rebbi Yehuda": {
+		"Type": "Sage",
+		"Power": 5,
+		"Era": "Tanna",
+	},
+	"Abaye": {
+		"Type": "Sage",
+		"Power": 4,
+		"Era": "Amora",
+	},
+	"Chezkiah": {
+		"Type": "Sage",
+		"Power": 3,
+		"Era": "Gaon",
+	},
+	"Rabbeinu Tam": {
+		"Type": "Sage",
+		"Power": 2,
+		"Era": "Rishon",
+	},
+	"Baal Shem Tov": {
+		"Type": "Sage",
+		"Power": 1,
+		"Era": "Acharon",
+	},
 	"Avraham": {
 		"Type": "Tanach",
 		"Effect": "Gain 3 actions"
