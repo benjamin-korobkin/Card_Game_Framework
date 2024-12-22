c2caa3da6b8d5bbf46e4fbb9b01362a04765ac48
benjamin-korobkin <benjamin.korobkin@gmail.com>
2024-12-23 00:34:42 +0200

    minor changes

--- src/new_card_game/Player.gd
+++ src/new_card_game/Player.gd
@@ -50,7 +50,7 @@ func draw_card():
 	if can_deduct_action():
 		if hand.is_full() and player_name=="Player2":
 			# TODO: Show that P2 is discarding
-			var rand_card = randi() % hand.hand_size
+			var rand_card = randi() % hand.hand_size - 1  # Bug fix?
 			hand.get_card(rand_card).move_to(cfc.NMAP.discard)
 			yield(hand.get_card(rand_card)._tween, "tween_all_completed")
 			draw_card()
